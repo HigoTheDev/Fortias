e0146b61c01a8636a696ad3d2cc26952fe072a9b
Phạm Vũ Lượng <luongphamvu2006@gmail.com>
2025-09-16 17:44:02 +0700

    di chuyen co ban

--- PlayerSpine.ts
+++ PlayerSpine.ts
@@ -1,36 +1,41 @@
-import {_decorator, Component, sp, input, Input, EventKeyboard, KeyCode, Vec3} from 'cc';
-const {ccclass, property} = _decorator;
+import { _decorator, Component, sp, input, Input, EventKeyboard, KeyCode, Vec2, RigidBody2D } from 'cc';
+const { ccclass, property } = _decorator;
 
-@ccclass('PlayerSpine')
-export class PlayerSpine extends Component{
+@ccclass('PlayerController')
+export class PlayerController extends Component {
     @property(sp.Skeleton)
     spine: sp.Skeleton = null!;
+
+    @property(RigidBody2D)
+    body: RigidBody2D = null!;
 
     @property
     speed: number = 200;
 
-    private moving: boolean = false;
-    private moveDir: Vec3 = new Vec3(0,0,0);
+    private moveDir: Vec2 = new Vec2(0, 0);
+    private originalScaleX: number = 1;
 
-    start(){
+    start() {
+        this.originalScaleX = this.node.getScale().x;
         this.spine.setAnimation(0, "idle", true);
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
         input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
-        this.originalScale = this.node.getScale();
     }
 
-    update(deltaTime: number) {
-        if (!this.moveDir.equals(Vec3.ZERO)) {
-            const pos = this.node.position;
-            const newPos = new Vec3(
-                pos.x + this.moveDir.x * this.speed * deltaTime,
-                pos.y + this.moveDir.y * this.speed * deltaTime,
-                pos.z
-            );
-            this.node.setPosition(newPos);
+    update() {
+        if (this.body) {
+            this.body.linearVelocity = new Vec2(this.moveDir.x * this.speed, this.moveDir.y * this.speed);
+        }
 
+        if (this.moveDir.x !== 0 || this.moveDir.y !== 0) {
             if (this.spine.animation !== "run") {
                 this.spine.setAnimation(0, "run", true);
+            }
+
+            if (this.moveDir.x > 0) {
+                this.node.setScale(this.originalScaleX, this.node.getScale().y, 1);
+            } else if (this.moveDir.x < 0) {
+                this.node.setScale(-this.originalScaleX, this.node.getScale().y, 1);
             }
         } else {
             if (this.spine.animation !== "idle") {
@@ -41,33 +46,19 @@ export class PlayerSpine extends Component{
 
     private onKeyDown(event: EventKeyboard) {
         switch (event.keyCode) {
-            case KeyCode.KEY_W:
-                this.moveDir.y = 1;
-                break;
-            case KeyCode.KEY_S:
-                this.moveDir.y = -1;
-                break;
-            case KeyCode.KEY_A:
-                this.moveDir.x = -1;
-                this.node.setScale(new Vec3(-this.originalScale.x, this.originalScale.y, this.originalScale.z));
-                break;
-            case KeyCode.KEY_D:
-                this.moveDir.x = 1;
-                this.node.setScale(new Vec3(this.originalScale.x, this.originalScale.y, this.originalScale.z));
-                break;
+            case KeyCode.KEY_W: this.moveDir.y = 1; break;
+            case KeyCode.KEY_S: this.moveDir.y = -1; break;
+            case KeyCode.KEY_A: this.moveDir.x = -1; break;
+            case KeyCode.KEY_D: this.moveDir.x = 1; break;
         }
     }
 
     private onKeyUp(event: EventKeyboard) {
         switch (event.keyCode) {
             case KeyCode.KEY_W:
-            case KeyCode.KEY_S:
-                this.moveDir.y = 0;
-                break;
+            case KeyCode.KEY_S: this.moveDir.y = 0; break;
             case KeyCode.KEY_A:
-            case KeyCode.KEY_D:
-                this.moveDir.x = 0;
-                break;
+            case KeyCode.KEY_D: this.moveDir.x = 0; break;
         }
     }
 }
